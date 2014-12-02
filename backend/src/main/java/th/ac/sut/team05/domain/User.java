493a31a4bededb49f933acc25c33198ae1f0e509
Mallika <mallika_siri@hotmail.com>
2014-12-03 01:12:55 +0700

    add User Entity

---
+++ backend/src/main/java/th/ac/sut/team05/domain/User.java
@@ -0,0 +1,25 @@
+package th.ac.sut.team05.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	
+}
