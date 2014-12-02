39a1fd0088b8a5dbb74d73436d782497e57be189
Jaris <rex_star@hotmail.com>
2014-12-02 23:29:20 +0700

    Entity Message

---
+++ backend/src/main/java/th/ac/sut/team05/domain/Message.java
@@ -0,0 +1,18 @@
+package th.ac.sut.team05.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+	
+@Id
+@GeneratedValue
+private Long id;
+
+@Column
+private String message;
+
+}
