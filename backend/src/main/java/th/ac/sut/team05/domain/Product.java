db4d204d0ea56d8c68fc3fd76fa0c2258feed7c3
kamthorn <zambogaibanez@gmail.com>
2014-12-02 23:07:56 +0700

    product,producttype

---
+++ backend/src/main/java/th/ac/sut/team05/domain/Product.java
@@ -0,0 +1,99 @@
+package th.ac.sut.team05.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private String idProduct;
+
+	@Column
+	private String name;
+
+	@Column
+	private int price;
+
+	@Column
+	private int amount;
+
+	@Column
+	private String detail;
+
+	@Column
+	private String picture;
+
+	@Column
+	private int discount;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIdProduct() {
+		return idProduct;
+	}
+
+	public void setIdProduct(String idProduct) {
+		this.idProduct = idProduct;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+}
