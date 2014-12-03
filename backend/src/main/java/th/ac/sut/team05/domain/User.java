cb9b1fdd97a59c47f8b934ce0cf52ebd7dc39431
Mallika <mallika_siri@hotmail.com>
2014-12-03 21:45:05 +0700

    modified User Entity

--- backend/src/main/java/th/ac/sut/team05/domain/User.java
+++ backend/src/main/java/th/ac/sut/team05/domain/User.java
@@ -1,5 +1,6 @@
 package th.ac.sut.team05.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,6 +14,95 @@ public class User {
 	@GeneratedValue
 	private long id;
 	
+	@Column
+	private String username;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private String name;
+	@Column
+	private String lastname;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String tel;
+	
+	@Column(columnDefinition = "ENUM('MALE','FEMALE')")
+	private String sex;
+	
+	
+	@Column(columnDefinition = "ENUM('ADMIN','USER')")
+	private String level;
+	
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
 
 	public long getId() {
 		return id;
